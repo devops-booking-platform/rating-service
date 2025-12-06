49738e68338ffc4719eef585c84a490d7400de85
Nebojša Vuga <nebojsavuga01@gmail.com>
2025-12-06 13:07:36 +0100

    feat(rating): Added domain entities

---
+++ RatingService/RatingService/Domain/AccommodationRating.cs
@@ -0,0 +1,18 @@
+﻿namespace RatingService.Domain;
+
+public class AccommodationRating : EntityWithGuidId
+{
+    public const int CommentMaxLength = 255;
+    public const int ViewingPropertiesMaxLength = 100;
+    public Guid AccommodationId { get; set; }
+    public Guid GuestId { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset? LastChangedAt { get; set; }
+    public int Rating { get; set; }
+    public string Comment { get; set; } = string.Empty;
+
+    // for viewing purpose
+    public string GuestFirstName { get; set; } = string.Empty;
+    public string GuestLastName { get; set; } = string.Empty;
+    public string GuestUsername { get; set; } = string.Empty;
+}
---
+++ RatingService/RatingService/Domain/HostRating.cs
@@ -0,0 +1,19 @@
+﻿namespace RatingService.Domain;
+
+public class HostRating : EntityWithGuidId
+{
+    public const int CommentMaxLength = 255;
+    public const int ViewingPropertiesMaxLength = 100;
+
+    public Guid HostId { get; set; }
+    public Guid GuestId { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset LastChangedAt { get; set; }
+    public int Rating { get; set; }
+    public string Comment { get; set; } = string.Empty;
+
+    // for viewing purpose
+    public string GuestFirstName { get; set; } = string.Empty;
+    public string GuestLastName { get; set; } = string.Empty;
+    public string GuestUsername { get; set; } = string.Empty;
+}
