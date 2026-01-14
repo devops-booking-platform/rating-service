fe5523ba984bcc027af98c8ed1c07618b3ae7618
Nebojša Vuga <nebojsavuga01@gmail.com>
2026-01-14 21:38:37 +0100

    fix: Added missing fields

--- RatingService/RatingService/Domain/DTOs/HostRatingResponse.cs
+++ RatingService/RatingService/Domain/DTOs/HostRatingResponse.cs
@@ -6,6 +6,9 @@ public class RatingResponse
     public int Rating { get; set; }
     public string Comment { get; set; } = string.Empty;
     public string GuestFullName { get; set; } = string.Empty;
+    public Guid GuestId { get; set; }
+    public Guid? HostId { get; set; }
+    public Guid? AccommodationId { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
     public DateTimeOffset? LastChangedAt { get; set; }
 }
