bf7b89d94fd86b6e204474748d45264baf9adee5
Bogdan Janosevic <96696175+BogdanJ33@users.noreply.github.com>
2026-01-19 19:31:41 +0100

    feat(tracing): Implemented rabbitmq tracing (#6)
    * feat(tracing): Implemented rabbitmq tracing
    
    * fix(tracing): Moved port to appsettings

--- RatingService/RatingService/Program.cs
+++ RatingService/RatingService/Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using OpenTelemetry;
+using OpenTelemetry.Context.Propagation;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using RatingService.Configuration;
 using RatingService.Data;
 using RatingService.Infrastructure;
@@ -13,6 +17,29 @@ builder.Host.UseSerilog((ctx, lc) => lc
     .ReadFrom.Configuration(ctx.Configuration)
     .Enrich.FromLogContext()
 );
+var compositeTextMapPropagator = new CompositeTextMapPropagator(new TextMapPropagator[]
+{
+    new TraceContextPropagator(),
+    new BaggagePropagator()
+});
+Sdk.SetDefaultTextMapPropagator(compositeTextMapPropagator);
+var otlpEndpoint = builder.Configuration["OpenTelemetry:OtlpExporter:Endpoint"];
+
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracing =>
+    {
+        tracing
+            .SetResourceBuilder(
+                ResourceBuilder.CreateDefault().AddService("ReservationService"))
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddRabbitMQInstrumentation()
+            .AddOtlpExporter(o =>
+            {
+                if (!string.IsNullOrWhiteSpace(otlpEndpoint))
+                    o.Endpoint = new Uri(otlpEndpoint);
+            });
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.Configure<RabbitMqSettings>(
